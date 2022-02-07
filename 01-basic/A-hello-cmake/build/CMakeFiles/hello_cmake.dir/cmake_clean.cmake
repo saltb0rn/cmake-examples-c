114b73ef9ce5730cdeb592470c7cf77217f8d1c9
Saltb0rn <asche34@outlook.com>
2022-02-07 18:05:44 +0800

    Init

---
+++ 01-basic/A-hello-cmake/build/CMakeFiles/hello_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cmake.dir/main.c.obj"
+  "CMakeFiles/hello_cmake.dir/main.c.obj.d"
+  "hello_cmake"
+  "hello_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01-basic/A-hello-cmake/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char* argsv[]) {
+  printf("Hello CMake!");
+  return 0;
+}
