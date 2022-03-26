a1ab15b339cb1aeb5e73af9f1765e11f502e3a1b
Saltb0rn <asche34@outlook.com>
2022-03-26 14:10:34 +0800

    Add an example for installing

---
+++ 01-basic/E-installing/src/Hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "installing/Hello.h"
+
+void print(Hello hi) {
+  printf("Hello Install");
+}
+
---
+++ 01-basic/E-installing/src/main.c
@@ -0,0 +1,7 @@
+#include "installing/Hello.h"
+
+int main(int argc, char *argv[]) {
+  Hello hi;
+  print(hi);
+  return 0;
+}
